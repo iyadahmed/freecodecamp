a4b26bc2da61ba4ddd33f0b64bbb61a681aab8cf
Iyad Ahmed <iyadahmed430@gmail.com>
2023-04-28 15:18:10 +0300

    Complete third project

--- budget.py
+++ budget.py
@@ -42,4 +42,42 @@ class Category:
 
 
 def create_spend_chart(categories: list[Category]):
-    pass
+    result = ""
+    result += "Percentage spent by category\n"
+
+    # Calculate total amount of withdrawals fo each category
+    withdrawals = [
+        sum(-1 * entry["amount"] for entry in c.ledger if entry["amount"] < 0)
+        for c in categories
+    ]
+    total = sum(withdrawals)
+    percentages = [(w / total) * 100 for w in withdrawals]
+    # Index of highest bar chart block for each category
+    bar_block_indices = [p // 10 for p in percentages]
+
+    for i in range(11)[::-1]:
+        line = ""
+        line += f"{i * 10:>3}| "
+        for j in bar_block_indices:
+            if i <= j:
+                line += "o"
+            else:
+                line += " "
+
+            line += "  "
+        result += line + "\n"
+
+    result += " " * 4 + "-" * (1 + 3 * len(categories)) + "\n"
+
+    for i in range(max(len(c.name) for c in categories)):
+        line = " " * 5
+        for c in categories:
+            if i < len(c.name):
+                line += c.name[i]
+            else:
+                line += " "
+            line += "  "
+        result += line + "\n"
+    
+    # Slice using [:-1] to remove the last newline character
+    return result[:-1]
